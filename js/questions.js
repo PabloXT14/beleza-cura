d70e224d6779408341f4b1eb61cae15816177995
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2022-01-31 17:07:43 -0300

    Start Design Project Freelancer

---
+++ js/questions.js
@@ -0,0 +1,45 @@
+/* Perguntas do questionário */
+
+let questions = [
+    {
+        id: 1,
+        question: "Caracterize o seu estilo	pessoal	atual (de acordo com o seu conhecimento atual sobre o assunto)",
+        options: [
+            'Prática',
+            'Discreta',
+            'Sofisticada',
+            'Feminina',
+            'Provocante',
+            'Fashionista',
+            'Original'
+        ]
+    },
+    {
+        id: 2,
+        question: "Como são as peças que predominam no seu guarda-roupa?",
+        options: [
+            "Confortáveis,	de	fácil	manutenção.",
+            "Atemporais,	corte	reto. (peças	que	nunca	saem	de	moda)",
+            "Tecidos	refinados,	corte	impecável.",
+            "Esvoaçantes,	com	tecidos	fluidos.",
+            "Justas,	com	decotes.",
+            "Aquelas	que	são	tendência	da	estação.",
+            "Vários	estilos	de	peças."
+        ]
+    }
+
+]
+
+
+
+/*
+Como são as	peças que predominam no	seu	guarda-roupa?
+
+Confortáveis,	de	fácil	manutenção.
+Atemporais,	corte	reto. (peças	que	nunca	saem	de	moda)
+Tecidos	refinados,	corte	impecável.
+Esvoaçantes,	com	tecidos	fluidos.
+Justas,	com	decotes.
+Aquelas	que	são	tendência	da	estação.
+Vários	estilos	de	peças.
+*/
